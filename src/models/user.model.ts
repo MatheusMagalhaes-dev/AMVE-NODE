ab140d5083f93364112ca94d465df9a07e3d7b28
matheusmagalhaes-dev <mahteu8@gmail.com>
2023-05-24 07:48:47 -0300

    user finish

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,32 +1,34 @@
-import { Document, Model, Schema } from "mongoose";
+import { Document, Model, model, Schema } from "mongoose";
 
 import { IUser } from "@ts";
 
 interface IUserDocument extends IUser, Document<string> {}
 interface IUserModel extends Model<IUserDocument> {}
 interface IUserMethods extends IUserDocument {
-    comparePassword(password: string): Promise<boolean>;
-    changeActive(): Promise<IUser>;
+  comparePassword(password: string): Promise<boolean>;
+  changeActive(): Promise<IUser>;
 }
 
 const UserSchema = new Schema<IUser, IUserModel, IUserMethods>(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        password: {
-            type: String,
-            required: true,
-            select: false,
-        },
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true,
-    }
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
+
+export const UserModel = model("User", UserSchema);
